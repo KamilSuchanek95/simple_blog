3cdd4590bc67b4645dd7b3a1d15bc29acd4a5467
Kamil Suchanek <kamilsuchanek95@gmail.com>
2021-02-25 00:21:58 +0100

    Add controllers => added action show to users/registration. fixed routes. now i have to fix show view.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :users
+
   root 'about#index'
   get 'about/index'
+
   resources :articles do
   	resources :comments
   end
+  devise_scope :user do 
+    get 'users/show' => 'users/registrations#show'
+    post 'users/show' => 'users/registrations#show'
+  end
+
+  Rails.application.routes.draw do
+      devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
+    end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
