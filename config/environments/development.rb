c2e3f54ffd9a8f7727580e4aa07302613e20b46b
Kamil Suchanek <kamilsuchanek95@gmail.com>
2021-02-24 13:13:27 +0100

    customize devise views and configuration mailer + add username do User.
    remove bulma builder, bulma in css only is ok. create avatar for logged users. 
    
    TODO: add buttons for user's system, check it, add user's avatars... hmmm

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,12 +32,22 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.perform_caching = false
+  config.action_mailer.perform_caching = true
   # enable sending
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    enable_starttls_auto: true,
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    user_name: "simple.blog.pisadlo@gmail.com",
+    password: "oh oh oh ciach (to nie jest hasło xd)",
+    authentication: 'plain'#,ssl: true
+  }
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
